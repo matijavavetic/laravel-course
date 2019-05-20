5a59275fa8ec20bf73ac69b21e509e33904a3fa3
matijavavetic <47482084+matijavavetic@users.noreply.github.com>
2019-05-20 21:17:22 +0200

    Routing

---
+++ myfirstsite/resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+</html>
+
+<body>
+    <h1>Contact Form</h1>
+</body>
+</html>
---
+++ myfirstsite/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+</html>
+
+<body>
+    <h1>Home Page</h1>
+
+    <p>
+        <a href="/contact">Contact us</a>
+    </p>
+</body>
+</html>
